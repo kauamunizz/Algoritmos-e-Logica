c4b7598c44971b347ff32dc968d58f5cf90ee742
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2023-08-01 15:31:29 -0300

    main

---
+++ Estrutura Sequencial/expressao_aritmetica.js
@@ -0,0 +1,34 @@
+// Operadores Aritmeticos no VisualG
+
+// Operador | Significado
+//        + | adição
+//        - | subtração
+//        * | multiplicação
+//        / | divisão
+//        \ | divisão inteira
+// % ou mod | resto da divisão (inteiro)
+//        ^ | potenciação (real)
+
+// Precedencia:
+// 1° lugar: ^
+// 2° lugar: * / \ %
+// 3° lugar: + -
+
+//Exemplos de expressoes aritmeticas:
+2 * 6 / 3
+//4
+
+3 + 2 * 4
+// 11
+
+(3 + 2) * 4
+// 20
+
+2 * 3 ^ 4
+//
+
+60 / (3 + 2) * 4
+//
+
+60 / ((3 + 2) * 4)
+//
---
+++ Estrutura Sequencial/variaveis.js
@@ -0,0 +1,22 @@
+// Declaração de variaveis
+
+//Sintaxe:
+// <nome> : <tipo>
+
+// ex:
+// idade : inteiro
+// altura : real
+// logradouro : caractere
+
+// Uma variavel possui:
+// Nome (ou identificador)
+// Tipo
+// Valor
+// Endereço
+
+// Tipos basicos em VisualG
+//         Tipo |                  Descrição | Valor padrão | Valores possiveis
+//      inteiro |             numero inteiro |            0 | -2147483648 a 2147483647
+//         real | numero com ponto flutuante |            0 | -1,4024E-37 a 3,4028E+38
+//    caractere |                      texto |           "" | (textos)
+//       logico |              valor verdade |        FALSO | FALSO, VERDADEIRO
